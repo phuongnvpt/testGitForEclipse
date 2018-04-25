be248faf0d381a1c08d72eee881a3d1cd562fb67
Phuong NV <ngvt.phuong88@yahoomail.com>
2018-04-25 22:09:08 +0700

    gg

--- TestGit/src/map/MapDemo.java
+++ TestGit/src/map/MapDemo.java
@@ -48,7 +48,7 @@ public class MapDemo {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("demo map aaaaa");
+		System.out.println("demo map a");
 		MapDemo demo = new MapDemo();
 		demo.initList();
 		demo.count(demo.getList());
